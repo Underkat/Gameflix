b67a67cb807d243e51eff98872aa20b636f575f5
Maxim Churzin <alienz280@gmail.com>
2022-08-16 17:04:47 -0400

    Row scrolling

--- components/Banner.js
+++ components/Banner.js
@@ -14,7 +14,7 @@ const Banner = ({ popular }) => {
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         {game ? (
-          <Image src={`${game.thumbnail}`} layout="fill" objectFit="cover" />
+          <Image src={`${game.thumbnail}`} layout="fill" objectFit="cover" className="blur-sm" />
         ) : (
           <Image
             src="https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
--- components/Row.js
+++ components/Row.js
@@ -1,12 +1,33 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import Thumbnail from "./Thumbnail";
+import { useRef, useState } from "react";
 
 const Row = ({ title, games }) => {
+  const rowRef = useRef(null);
+  const [isMoved, setIsMoved] = useState(false);
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth /2
+          : scrollLeft + clientWidth /2;
+
+      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className="space-y-0.5 h-36 transition-all duration-[.2s]
+    <div
+      className="space-y-0.5 h-36 transition-all duration-[.2s]
     md:space-y-2 md:h-52
     sm:h-36 
-    lg:h-52">
+    lg:h-52"
+    >
       <h2
         className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] 
       transition duration-200 hover:text-white md:text-2xl"
@@ -15,19 +36,24 @@ const Row = ({ title, games }) => {
       </h2>
       <div className="group relative md:-ml-2">
         <ChevronLeftIcon
-          className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 
-        cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 
+        cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${!isMoved && "hidden"}`}
+          onClick={() => handleClick("left")}
         />
 
-        <div className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
-          {games.slice(0, 10).map((game) => (
+        <div
+          ref={rowRef}
+          className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
+        >
+          {games.slice(0, 20).map((game) => (
             <Thumbnail key={game.id} game={game} />
           ))}
         </div>
 
         <ChevronRightIcon
-          className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer 
-          opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer 
+          opacity-0 transition hover:scale-125 group-hover:opacity-100`}
+          onClick={() => handleClick("right")}
         />
       </div>
     </div>
